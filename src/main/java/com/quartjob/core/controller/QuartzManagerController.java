323cd03c5b939a1402b97ae3d207978b3122a390
zhangjianfang <zhangjf@qdcares.cn>
2020-08-12 13:40:37 +0800

    修改部分代码，解决错误问题

--- src/main/java/com/quartjob/core/controller/QuartzManagerController.java
+++ src/main/java/com/quartjob/core/controller/QuartzManagerController.java
@@ -1,20 +1,13 @@
 package com.quartjob.core.controller;
 
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.quartjob.core.dao.quartz.AmosQuartzDao;
-import com.quartjob.core.dto.quart.QuartzJob;
 import com.quartjob.core.entity.quartz.JobEntity;
 import com.quartjob.core.service.QuartJobService;
 import com.quartjob.core.utils.BizExceptionEnum;
 import com.quartjob.core.utils.ConstantMsgs;
 import com.quartjob.core.utils.ResponseEntity;
-import org.quartz.*;
-import org.quartz.impl.StdSchedulerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("quartzManager")
@@ -32,7 +25,7 @@ public class QuartzManagerController {
      * @Title: QuartzManagerController.java
      * @Copyright: Copyright (c) 2014
      *
-     * @author Comsys-LZP
+     * @author zjf
      * @date 2014-6-26 下午03:47:44
      * @version V2.0
      */
@@ -63,7 +56,7 @@ public class QuartzManagerController {
      * @Title: QuartzManagerController.java
      * @Copyright: Copyright (c) 2014
      *
-     * @author Comsys-LZP
+     * @author zjf
      * @date 2014-6-26 下午03:49:21
      * @version V2.0
      */
@@ -80,7 +73,7 @@ public class QuartzManagerController {
      * @Title: QuartzManagerController.java
      * @Copyright: Copyright (c) 2014
      *
-     * @author Comsys-LZP
+     * @author zjf
      * @date 2014-6-26 下午03:49:51
      * @version V2.0
      */
@@ -96,44 +89,22 @@ public class QuartzManagerController {
      * @Title: QuartzManagerController.java
      * @Copyright: Copyright (c) 2014
      *
-     * @author Comsys-LZP
+     * @author zjf
      * @date 2014-6-26 下午03:50:18
      * @version V2.0
      */
     @PostMapping("startJobs")
     public ResponseEntity startJobs(@RequestBody JobEntity jobEntity) {
-        JobEntity job = amosQuartzDao.selectById(jobEntity.getId());
+        JobEntity job = quartJobService.getById(jobEntity.getId());
         if(job == null){
             return new ResponseEntity(ConstantMsgs.FAIL, BizExceptionEnum.UNKNOWN_ENTITY.getCode(), BizExceptionEnum.UNKNOWN_ENTITY.getMsg());
         }
-        addToScheduler(job);
+        quartJobService.startJob(job);
         return new ResponseEntity(ConstantMsgs.SUCCESS);
-    }
-    /**
-     * @Description:关闭所有定时任务
-     *
-     *
-     * @Title: QuartzManagerController.java
-     * @Copyright: Copyright (c) 2014
-     *
-     * @author Comsys-LZP
-     * @date 2014-6-26 下午03:50:26
-     * @version V2.0
-     */
-    @PostMapping("shutdownJobs")
-    public void shutdownJobs() {
-        try {
-            Scheduler sched = gSchedulerFactory.getScheduler();
-            if (!sched.isShutdown()) {
-                sched.shutdown();
-            }
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
     }
 
     @GetMapping("test")
     public void test(){
-        System.out.println("定时任务测试...");
+        System.out.println("定时任务测试，当前时间为：" + System.currentTimeMillis());
     }
 }
